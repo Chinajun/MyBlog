65d2f35858349b96535dc18f90f248e93ed2017a
Chinajun <398264427@qq.com>
2020-09-29 19:40:00 +0800

    移动端背景图片

--- ux/src/App.vue
+++ ux/src/App.vue
@@ -20,9 +20,16 @@ export default {
   },
   mounted() {
     this.path = this.$route.path;
+    // 判断设备
+    if (this.isMobile()) {
+      // 手机端背景图片横向不平铺，设定min-height
+      document.body.style.backgroundSize = '100%';
+    } else {
+      // pc端
+    }
     // 随机更换背景图片
     var index =parseInt(Math.random()*9);
-    this.bgImg = "/static/bg"+index+"_tiny.jpg";
+    this.bgImg = "/static/bg"+index+".jpg";
     document.body.style.backgroundImage = "url("+this.bgImg+")";
   },
   watch:{
@@ -30,6 +37,13 @@ export default {
       this.path = to.path
     }
   },
+  methods:{
+    // 识别手机端还是pc端
+    isMobile() {
+      let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+      return flag;
+    },
+  }
 }
 </script>
 
