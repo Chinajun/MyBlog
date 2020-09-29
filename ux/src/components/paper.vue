808407efaadce54eb92090b084a82387532f5943
cyj <398264427@qq.com>
2020-09-29 10:51:19 +0800

    测试3

--- ux/src/components/paper.vue
+++ ux/src/components/paper.vue
@@ -64,7 +64,7 @@
         this.$router.push({path:"articleDetail",query:{Id:item.Id}})
       },
       getAllArticle(){
-        axios.post("/index.php/blog/getArticle",{
+        axios.post("/public/index.php/blog/getArticle",{
           page:this.page
         }).then((response) => {
           // this.paperList = [];
