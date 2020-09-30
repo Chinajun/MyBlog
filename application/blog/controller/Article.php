ad3594adbac9b351b52e7356498a34357c890cb9
Chinajun <398264427@qq.com>
2020-09-30 09:21:09 +0800

    修改文章上传图片路径

--- application/blog/controller/Article.php
+++ application/blog/controller/Article.php
@@ -87,11 +87,12 @@ class Article extends Controller
     public function uploadImg(){
         $file = request()->file("file");
         if($file){
-            $info = $file->validate(['ext' => 'jpg,jpeg'])->rule('md5')->move(ROOT_PATH . 'public/static');
+//            $info = $file->validate(['ext' => 'jpg,jpeg'])->rule('md5')->move(ROOT_PATH . 'public/static');
+            $info = $file->validate(['ext' => 'jpg,jpeg'])->rule('md5')->move(ROOT_PATH . 'ux/static');
             if($info){
                 $fileSaveName = $info->getSaveName();
 //                $fileSaveName = 'http://localhost/public/static/'.str_replace("\\",'/',$fileSaveName);
-                $fileSaveName = 'http://47.98.213.131/public/static/'.str_replace("\\",'/',$fileSaveName);
+                $fileSaveName = 'http://47.98.213.131/static/'.str_replace("\\",'/',$fileSaveName);
                 return [
                     'code' => 0,
                     'msg' => '图片上传成功',
